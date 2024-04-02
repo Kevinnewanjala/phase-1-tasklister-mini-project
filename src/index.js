f9913bef316bf6d18fe52e6e4bb5f71af054ea7b
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-04-02 08:05:35 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskForm = document.querySelector('form');
+
+  newTaskForm.addEventListener('submit', handleFormSubmit);
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    const newToDo = e.target.querySelector('#new-task-description').value;
+    buildToDoElement(newToDo);
+    newTaskForm.reset();
+  }
+
+  function buildToDoElement(task) {
+    const taskLi = createTaskElement(task);
+    const deleteButton = createDeleteButton();
+    taskLi.appendChild(deleteButton);
+    document.querySelector('#tasks').appendChild(taskLi);
+    deleteButton.addEventListener('click', () => {
+      taskLi.remove();
+    });
+  }
+
+  function createTaskElement(task) {
+    const taskLi = document.createElement('li');
+    taskLi.textContent = task;
+    return taskLi;
+  }
+
+  function createDeleteButton() {
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'x';
+    return deleteButton;
+  }
 });
